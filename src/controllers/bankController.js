93dac7b7917dae5ae42fe5099a94e94d5c484a63
rahullkr <rk7318323176@gmail.com>
2023-10-07 18:53:46 +0530

    integrated jwt token

--- src/controllers/bankController.js
+++ src/controllers/bankController.js
@@ -1,11 +1,17 @@
 const express = require("express");
-const BankModel = require('../models/bankModels');
+const BankModel = require("../models/bankModels");
+const jwt = require("jsonwebtoken");
 
-const registerUser = async (req,res)=>{
+const generateToken = (id) => {
+  //1. what do you want to create the token with, 2. the tool with the help of which it will create the token, 3. expiring time
+  return jwt.sign({ id }, process.env.JWT_SECRETS, { expiresIn: "1d" });
+};
+
+const registerUser = async (req, res) => {
   // for register, you have to check 3 things
-  // 1- if either of them is not provided return error; 
+  // 1- if either of them is not provided return error;
   // 2- if email exists
-  // 3- if password satisfies the condition 
+  // 3- if password satisfies the condition
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     res.status(400);
@@ -15,7 +21,6 @@ const registerUser = async (req,res)=>{
     res.status(400);
     throw new Error("password must be of 8 characters");
   }
-
 
   const userExist = await BankModel.findOne({ email });
   if (userExist) {
@@ -28,20 +33,19 @@ const registerUser = async (req,res)=>{
     email,
     password,
   });
-
-    if (user) {
+  const token = generateToken(user._id);
+  if (user) {
     const { name, email } = user;
     res.status(201).json({
-     name, 
-     email,
-
+      name,
+      email,
+      
     });
   } else {
     res.status(400);
     throw new Error("invalid user data");
   }
- }
-
+};
 
 const getBankDetails = (req, res) => {
   const id = req.params.id;
